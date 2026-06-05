32767eb1f73d3624ec8d13d81ee97c1d4b8d2845
Wujay <73167607+wujie272@users.noreply.github.com>
2026-06-05 09:45:14 +0800

    fix: update NDK version to 28.2.13676358 for jni plugin compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,8 +9,7 @@ plugins {
 android {
     namespace = "com.psyche.kelivo"
     compileSdk = flutter.compileSdkVersion
-//    ndkVersion = flutter.ndkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
